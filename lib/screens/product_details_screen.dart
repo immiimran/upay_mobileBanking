68b29efffade547b0a4e3ca9af806b523c4e6c52
Imran Immi <imrnimmi@gmail.com>
2025-05-15 22:07:36 +0600

    update

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -5,6 +5,6 @@ class ProductDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(body: Center(child: Text("Service Details Screen")));
   }
 }
